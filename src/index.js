46b30be4a080427f09c4471f118e93594b86fbc4
Devin <ospreywebservices@gmail.com>
2021-08-16 12:01:59 -0400

    added js for 'travel' dropdown functions. Need to add CSS and window click functions for toggles.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello');
+let itemTwo = document.getElementById('bannerItem2');
+let itemTwoContainer = document.getElementById('itemTwoContainer');
+itemTwo.addEventListener('mouseover', () => {
+  itemTwoContainer.classList.toggle('show');
+  console.log('worked');
+});
